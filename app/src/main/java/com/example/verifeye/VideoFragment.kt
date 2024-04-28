ef61ca5dd4bc1fa0df1f4e809fe97e9f6c6cf3fc
CodingForFun-coder <prangel2@toromail.csudh.edu>
2024-04-27 23:24:51 -0700

    Users can now access preventive facts in articles and YouTube videos, take a quiz, and access resources to improve their knowledge of bias detection.

--- app/src/main/java/com/example/verifeye/VideoFragment.kt
+++ app/src/main/java/com/example/verifeye/VideoFragment.kt
@@ -11,17 +11,10 @@ import androidx.fragment.app.Fragment
 
 class VideoFragment : Fragment() {
 
-    private var param1: String? = null
-    private var param2: String? = null
     private var webView: WebView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
 
     }
 
